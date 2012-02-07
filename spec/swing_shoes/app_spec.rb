44963268ebc22dee8192d23f79811d4e9433e963
pfitzgibbons <peter.fitzgibbons@gmail.com>
2012-02-07 17:46:31 -0600

    Remove blocking calls (GUI Visible) from SwingShoes:App shared exmaples

--- spec/swing_shoes/app_spec.rb
+++ spec/swing_shoes/app_spec.rb
@@ -1,13 +1,26 @@
 require "spec_helper"
 require "spec/swing_shoes/spec_helper"
 
+    import javax.swing.JFrame
+
 describe SwingShoes::App do
 
-  it_behaves_like "A WhiteShoes Shoes::App"
+  context "White Shoes Shared Examples" do
 
+    let(:mock_pane) { mock(:content_pane, :setSize => true ) }
 
-  after :each do
-    subject
+    before :each do
+      JFrame.any_instance.stub(:get_content_pane).
+        and_return mock_pane
+      JFrame.any_instance.stub(:set_visible)
+    end
+
+    after :each do
+      #subject.gui_container.close
+    end
+
+    it_behaves_like "A WhiteShoes Shoes::App"
+
   end
 
 end
